5d5c7eccb6853fcb5a5a88e54913d07801da00b5
WANGZijian1994 <wang zijian@10.8.21.249>
2018-11-08 18:57:52 +0100

    小测试

---
+++ Test/src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		char ch='s';
+		char sh='t';
+		System.out.print(ch+sh+"是s和t对应ASCII码值的和");
+		System.out.print("\n"+"s and t: "+ch+sh);
+
+	}
+
+}
