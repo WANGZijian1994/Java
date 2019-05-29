2f2b3724aad089548804b224cfad2731f9327c47
zijian <zijian@192.168.0.26>
2019-05-29 18:36:02 +0200

    String and Array

---
+++ Test/src/StringMethods/StringAndChar.java
@@ -0,0 +1,12 @@
+package StringMethods;
+
+public class StringAndChar {
+
+	public static void main(String[] args) {
+		String str = "abc";
+		char data[] = {'a','b','c'};
+		String str1 = new String(data);
+		System.out.println(str);
+		System.out.println(str1);
+	}
+}
