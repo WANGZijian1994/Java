c8b2c4bc89c7f4e122dc12b48770cfddb1ef30e3
zijian <zijian@192.168.3.219>
2019-07-06 16:00:30 +0200

    Palindrome

--- Test/src/Caesar_Cipher/Palindrome.java
+++ Test/src/Caesar_Cipher/Palindrome.java
@@ -10,7 +10,29 @@ public class Palindrome {
 		return s1;
 	}
 	
+	public static String Reverse2(String s) {
+		String s1 = new String();
+		for(int i = 0;i<s.length();i++){
+			s1 = s.charAt(i)+s1;
+		}
+		return s1;
+	}
+	
+	public static boolean isPalinDrome(String s) {
+		String reverse = new String();
+		String original = s.toLowerCase();
+		original = original.replaceAll("[^A-Za-z]", "");
+		for(int i = original.length()-1;i>=0;i--) {
+			char ch = original.charAt(i);
+			reverse+=ch;
+		}
+		System.out.println(reverse + ":"+original);
+		return reverse.equals(original);
+	}
+	
 	public static void main(String[] args) {
 		System.out.println(Reverse("CBA"));
+		System.out.println(Reverse2("CBA"));
+		System.out.print(isPalinDrome("Etto Otte"));
 	}
 }
